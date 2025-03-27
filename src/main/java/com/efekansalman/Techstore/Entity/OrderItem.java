d5881ca052394862e4bc727e814cd314b9b46423
Efekan <75560188+EfekanSalman@users.noreply.github.com>
2025-03-27 22:56:19 +0300

    Initial commit

---
+++ src/main/java/com/efekansalman/Techstore/Entity/OrderItem.java
@@ -0,0 +1,33 @@
+package com.efekansalman.Techstore.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "order_items")
+@Data
+@NoArgsConstructor
+public class OrderItem {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "order_id", nullable = false)
+	private Order order;
+	
+	@ManyToOne
+	@JoinColumn(name = "product_id", nullable = false)
+	private Product product;
+	
+	@Column(nullable = false)
+	private int quantity;
+}
---
+++ src/main/java/com/efekansalman/Techstore/Entity/Product.java
@@ -0,0 +1,41 @@
+package com.efekansalman.Techstore.Entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false)
+	private BigDecimal price;
+	
+	@Column(nullable = false)
+	private int stock;
+	
+	@ManyToOne
+	@JoinColumn(name = "category_id", nullable = false)
+	private Category category; 
+}
